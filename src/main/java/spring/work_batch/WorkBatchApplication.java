c84cba3e1650445bbf75400abafd934bd2e45513
dmswo <dmswo106@naver.com>
2025-01-30 23:10:59 +0900

    Initial commit

---
+++ src/main/java/spring/work_batch/WorkBatchApplication.java
@@ -0,0 +1,13 @@
+package spring.work_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/work_batch/WorkBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.work_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
