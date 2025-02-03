89430865b3406fb162c6e0e5e1bf100525641930
dmswo <dmswo106@naver.com>
2025-02-03 22:24:18 +0900

    스프링 배치 jobinstance 공부

---
+++ src/main/java/spring/work_batch/JobInstanceConfiguration.java
@@ -0,0 +1,42 @@
+package spring.work_batch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobInstanceConfiguration {
+    @Bean
+    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("job", jobRepository)
+                .start(step1(jobRepository, transactionManager))
+                .next(step2(jobRepository, transactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step1", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("helloStep1 executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
+
+    @Bean
+    public Step step2(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("step2", jobRepository)
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("helloStep2 executed");
+                    return RepeatStatus.FINISHED;
+                }, transactionManager).build();
+    }
+}
