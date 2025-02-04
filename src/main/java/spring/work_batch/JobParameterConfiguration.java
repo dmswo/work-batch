88ec25f6b3b4598acbe04783c3af7e67ab7e11c5
dmswo <dmswo106@naver.com>
2025-02-04 21:32:59 +0900

    스프링 배치 jobParameter 공부

--- src/main/java/spring/work_batch/JobParameterConfiguration.java
+++ src/main/java/spring/work_batch/JobParameterConfiguration.java
@@ -2,6 +2,7 @@ package spring.work_batch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
@@ -11,12 +12,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.Map;
+
 @Configuration
 @RequiredArgsConstructor
-public class JobInstanceConfiguration {
+public class JobParameterConfiguration {
     @Bean
-    public Job job(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        return new JobBuilder("job", jobRepository)
+    public Job BatchJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new JobBuilder("Job", jobRepository)
                 .start(step1(jobRepository, transactionManager))
                 .next(step2(jobRepository, transactionManager))
                 .build();
@@ -26,6 +29,18 @@ public class JobInstanceConfiguration {
     public Step step1(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("step1", jobRepository)
                 .tasklet((contribution, chunkContext) -> {
+                    JobParameters jobParameters = contribution.getStepExecution().getJobParameters();
+                    jobParameters.getString("name");
+                    jobParameters.getLong("seq");
+                    jobParameters.getDate("date");
+                    jobParameters.getDouble("age");
+
+                    Map<String, Object> jobParameters1 = chunkContext.getStepContext().getJobParameters();
+                    jobParameters1.get("name");
+                    jobParameters1.get("seq");
+                    jobParameters1.get("date");
+                    jobParameters1.get("age");
+
                     System.out.println("helloStep1 executed");
                     return RepeatStatus.FINISHED;
                 }, transactionManager).build();
